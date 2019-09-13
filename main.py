d771988ea5cd60b82ac03a8b6141dd20d3d0ba8e
JennAnnDag7 <jenniferd7@hotmail.com>
2019-09-12 20:50:34 -0400

    started caesar

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+form="""
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+    <form method="post">
+        <label>
+            Rotate by:
+        </label>
+        <input type="text" placeholder="0" name="rot"/>
+        <textarea name="text"></textarea>
+        <input type="submit" value="Submit Query"/>
+    </body>
+</html>"""
+@app.route("/")
+def index():
+    return form.format("")
+
+app.run()
